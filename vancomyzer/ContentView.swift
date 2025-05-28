88d46512ceca245da125cba2a073609b4761196a
Mario Cardenas <marioce@hotmail.com>
2025-05-28 15:19:58 -0500

    Initial Commit

---
+++ vancomyzer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  vancomyzer
+//
+//  Created by Mario Cardenas on 5/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ vancomyzer/vancomyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  vancomyzerApp.swift
+//  vancomyzer
+//
+//  Created by Mario Cardenas on 5/28/25.
+//
+
+import SwiftUI
+
+@main
+struct vancomyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
